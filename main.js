ce4b796c0f8f6e8504fcea851f36f805a1d9f10f
Aman Agrawal <52325823+200198amanagrawal@users.noreply.github.com>
2020-02-04 22:53:26 +0530

    node program

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+var main=require("./second");
+console.log(__dirname);
+console.log("Accessing name from the second:"+main.name);
---
+++ second.js
@@ -0,0 +1,3 @@
+console.log("hello from second");
+var name="aman";
+module.exports.name=name;
