ea1782e90aa529f1686dcebaabed4814ee612c22
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-04 23:42:08 +0530

    class usage

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ console.log("hello world");
 var main=require("./second");
 console.log(__dirname);
 console.log("Accessing name from the second:"+main.name);
+class User{
+    constructor()
+    {
+        this.name="Aman Agrawal";
+        this.age=22;
+    }
+    getName()
+    {
+        return this.name;
+    }
+}
+var user=new User();
+console.log(user.getName());
+console.log(user.age);
